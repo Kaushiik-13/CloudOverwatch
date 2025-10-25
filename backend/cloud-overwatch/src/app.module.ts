76c1abf82e2d3b5d6f0ec23a054efef0e1b0feab
Kaushiik A <akkaushiik@gmail.com>
2025-10-25 22:16:11 +0530

    final commit

--- backend/cloud-overwatch/src/app.module.ts
+++ backend/cloud-overwatch/src/app.module.ts
@@ -8,15 +8,15 @@ import { AwsModule } from './aws/aws.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'cloudoverwatch.c9eys0q08frx.ap-south-1.rds.amazonaws.com',
-      port: 3306,
-      username: 'admin', // your MySQL username
-      password: 'admin12345678', // your MySQL password
-      database: 'cloudoverwatch',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT!, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [User],
-      synchronize: false, // ✅ don't overwrite your existing schema
+      synchronize: false,
     }),
-    AuthModule,AwsModule
+    AuthModule, AwsModule
   ],
 })
-export class AppModule {}
+export class AppModule { }
